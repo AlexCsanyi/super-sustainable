da4f8283a04a67352079834facfa7d8eefc9e7a7
AlexCsanyi <alex.csanyi@gmail.com>
2020-12-27 19:08:23 +0000

    Add components

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+import Footer from "../components/footer";
+import Meta from "../components/meta";
+import Nav from "./nav";
+
+export default function Layout({ preview, children }) {
+  return (
+    <>
+      <Meta />
+      <Nav />
+      <div className="min-h-screen">
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  );
+}
