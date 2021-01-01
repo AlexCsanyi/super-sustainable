0872b187ab375c83daa54315acf7ca3b392dbf10
Alex Csanyi <alex.csanyi@gmail.com>
2021-01-01 10:11:33 +0000

    readjust pages as nav is now added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import { Moon, Sun } from "react-feather";
 import Container from "../components/container";
 import MoreStories from "../components/more-stories";
 import HeroPost from "../components/hero-post";
@@ -10,20 +7,8 @@ import Layout from "../components/layout";
 import { getAllPosts } from "../lib/api";
 
 export default function Home({ allPosts }) {
-  const { theme, setTheme } = useTheme();
-  const [isMounted, setIsMounted] = useState(false);
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  const switchTheme = () => {
-    if (isMounted) {
-      setTheme(theme === "light" ? "dark" : "light");
-    }
-  };
 
   return (
     <>
@@ -32,11 +17,6 @@ export default function Home({ allPosts }) {
           <Head>
             <title>Super Sustainable</title>
           </Head>
-          <div className="flex justify-end pt-4 items-center">
-            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/AlexCsanyi" className="text-primary pr-2 font-bold cursor-pointer hover:text-accent">Twitter<span className="text-accent font-extrabold">.</span></a>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/AlexCsanyi" className="text-primary pr-2 font-bold cursor-pointer hover:text-accent">GitHub<span className="text-accent font-extrabold">.</span></a>
-            {theme === 'light' ? <Moon className="text-accent cursor-pointer" size={32} onClick={switchTheme} /> : <Sun className="text-accent cursor-pointer" size={32} onClick={switchTheme} />}
-          </div>
           <Intro />
           {heroPost && (
             <HeroPost
