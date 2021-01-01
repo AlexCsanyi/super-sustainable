65b7d75a9429440965afeb730ba6ec260fb8ce7d
Alex Csanyi <alex.csanyi@gmail.com>
2021-01-01 10:10:52 +0000

    add nav component

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,18 @@
 import Footer from "../components/footer";
 import Meta from "../components/meta";
+import Nav from "../components/nav";
 
 export default function Layout({ preview, children }) {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Meta />
+            <div className="container mx-auto px-5 mt-5 mb-20">
+                <Nav />
+            </div>
+            <div className="min-h-screen">
+                <main>{children}</main>
+            </div>
+            <Footer />
+        </>
+    );
 }
---
+++ components/nav.js
@@ -0,0 +1,32 @@
+import Header from './header'
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "react-feather";
+import { useRouter } from 'next/router';
+
+export default function Nav() {
+  const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const switchTheme = () => {
+    if (isMounted) {
+      setTheme(theme === "light" ? "dark" : "light");
+    }
+  };
+
+  return (
+    <div className={`flex ${router.pathname !== '/' ? 'justify-between' : 'justify-end'} h-10 items-center`}>
+        {router.pathname !== '/' ? <Header /> : ''}
+        <div className="flex items-center">
+            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/AlexCsanyi" className="text-primary pr-2 font-bold leading-8 cursor-pointer hover:text-accent">Twitter<span className="text-accent font-extrabold">.</span></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/AlexCsanyi" className="text-primary pr-2 font-bold leading-8 cursor-pointer hover:text-accent">GitHub<span className="text-accent font-extrabold">.</span></a>
+            {theme === 'light' ? <Moon className="text-accent cursor-pointer" size={32} onClick={switchTheme} /> : <Sun className="text-accent cursor-pointer" size={32} onClick={switchTheme} />}
+        </div>
+    </div>
+  )
+}
