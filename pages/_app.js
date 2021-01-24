b633349af1f0d95884ca33a14bb9dc50bff42f8d
Alex Csanyi <alex.csanyi@gmail.com>
2021-01-24 19:45:35 +0000

    prism for code highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import "prismjs/themes/prism-dark.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider attribute="class">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
